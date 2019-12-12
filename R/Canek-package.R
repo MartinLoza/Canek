ae94595b5a44096fb29d38b52ae06bc28e4a273b
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2019-12-12 10:53:55 +0900

    Change to ImportFrom

--- R/Canek-package.R
+++ R/Canek-package.R
@@ -5,6 +5,11 @@
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 ## usethis namespace: end
-#' @import FNN irlba tictoc numbers fpc ape spdep
+#' @import  tictoc numbers
 #' @importFrom stats dist kmeans sd
+#' @importFrom irlba prcomp_irlba
+#' @importFrom fpc pamk
+#' @importFrom ape mst
+#' @importFrom spdep Rotation
+#' @importFrom FNN get.knnx
 NULL
