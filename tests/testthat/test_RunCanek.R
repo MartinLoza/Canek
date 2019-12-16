7713d53f7e3693e546e063ed30bff34ac98b5ce7
Diego Diez <diego10ruiz@gmail.com>
2019-12-16 12:14:32 +0900

    Add unit tests for RunCanek() on SingleCellExperiment and list objects.

--- tests/testthat/test_RunCanek.R
+++ tests/testthat/test_RunCanek.R
@@ -1,17 +1,36 @@
 context("test-RunCanek")
 
+# Create Seurat object.
 x <- lapply(names(SimBatches$batches), function(batch) {
   Seurat::CreateSeuratObject(SimBatches$batches[[batch]], project = batch)
 })
 x <- merge(x[[1]], x[[2]])
+
+# Create SingleCellExperiment object.
+y <- Seurat::as.SingleCellExperiment(x)
+
+# RunCanek.
 x <- RunCanek(x, "orig.ident")
+y <- RunCanek(y, "orig.ident")
+z <- RunCanek(SimBatches$batches)
 
 test_that("RunCanek works on Seurat objects", {
   expect_false(is.null(x))
   expect_is(x, "Seurat")
-  expect_equal(Seurat::Assays(x), 2)
+  expect_length(Seurat::Assays(x), 2)
   expect_equal(Seurat::Assays(x), c("RNA", "Canek"))
 })
-  expect_equal(length(x@assays), 2)
-  expect_equal(names(x@assays), c("RNA", "Canek"))
+
+test_that("RunCanek works on SingleCellExperiment objects", {
+  expect_false(is.null(y))
+  expect_is(y, "SingleCellExperiment")
+  expect_length(SummarizedExperiment::assays(y), 3)
+  expect_equal(names(SummarizedExperiment::assays(y)), c("counts", "logcounts", "Canek"))
+})
+
+test_that("RunCanek works on lists", {
+  expect_false(is.null(z))
+  expect_is(z, "list")
+  expect_length(z, 2)
+  expect_equal(names(z), c("B1/B2", "Batches Integrated"))
 })
