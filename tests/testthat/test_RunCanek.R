0f2745712839a640a2efe1d58bcfd4882f21ba1a
Diego Diez <diego10ruiz@gmail.com>
2019-12-16 12:04:45 +0900

    Use accessor functions for Seurat unit tests.

--- tests/testthat/test_RunCanek.R
+++ tests/testthat/test_RunCanek.R
@@ -6,9 +6,12 @@ x <- lapply(names(SimBatches$batches), function(batch) {
 x <- merge(x[[1]], x[[2]])
 x <- RunCanek(x, "orig.ident")
 
-test_that("RunCanek works", {
+test_that("RunCanek works on Seurat objects", {
   expect_false(is.null(x))
   expect_is(x, "Seurat")
+  expect_equal(Seurat::Assays(x), 2)
+  expect_equal(Seurat::Assays(x), c("RNA", "Canek"))
+})
   expect_equal(length(x@assays), 2)
   expect_equal(names(x@assays), c("RNA", "Canek"))
 })
