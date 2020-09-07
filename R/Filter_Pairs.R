6b38b421c5e04d49627dc5ab41a55255fb439858
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2020-09-07 14:16:50 +0900

    Filter pairs by quartiles

--- R/Filter_Pairs.R
+++ R/Filter_Pairs.R
@@ -18,81 +18,123 @@
 #'
 Pairs_Selection <- function(B1,B2, Pairs, Num_Clusters = 1, Verbose = FALSE){
 
-  if(Verbose){
-    cat("\n\nSelecting Pairs by Clusters")
-    cat(paste('\n\tClusters for selecting pairs:',Num_Clusters))
-  }
+  # if(Type == "Clustering"){
 
-  #Initialization of varibles for clustering and selection of cell pairs
-  Mu <- matrix(0L, nrow = Num_Clusters, ncol = 1)
-  SD <- matrix(0L, nrow = Num_Clusters, ncol = 1)
+    # if(Verbose){
+    #   cat("\n\nSelecting Pairs by Clusters")
+    #   cat(paste('\n\tClusters for selecting pairs:',Num_Clusters))
+    # }
+    #
+    # #Initialization of varibles for clustering and selection of cell pairs
+    # Mu <- matrix(0L, nrow = Num_Clusters, ncol = 1)
+    # SD <- matrix(0L, nrow = Num_Clusters, ncol = 1)
+    #
+    # # Clustering to select pairs
+    # #Cluster <- kmeans(t(B2)[,1:10],Num_Clusters)
+    # Cluster <- kmeans(t(B2),Num_Clusters)
+    #
+    # #Initialization of variables for the correction
+    # Pairs_Dim <- dim(Pairs)
+    # Num_Pairs <- Pairs_Dim[1]
+    # Pairs <- matrix(c(as.vector(Pairs[1:Num_Pairs,1]), as.vector(Pairs[1:Num_Pairs,2])), nrow = Num_Pairs)
+    # #Samples <- Pairs
+    #
+    # for (i in 1:Num_Clusters) {
+    #
+    #   #Cluster cells index
+    #   Cl_Cells_Index<- which(Cluster$cluster ==i)
+    #
+    #   #Cluster cells subset
+    #   Cl_Cells<- B2[,Cl_Cells_Index]
+    #
+    #   #Cluster cells pairs
+    #   Cl_Pairs_Index <- which(Pairs[,1]==Cl_Cells_Index[1])
+    #   for (j in 2:length(Cl_Cells_Index) ) {
+    #     Cl_Pairs_Index <- c(Cl_Pairs_Index,which(Pairs[,1]==Cl_Cells_Index[j]))
+    #   }
+    #
+    #   #Subset of the pairs corresponding to the cluster
+    #   Cl_Pairs <- Pairs[Cl_Pairs_Index,]
+    #
+    #   #Number of pairs for this cluster
+    #   Pairs_Dim <- dim(Cl_Pairs)
+    #   Num_Pairs <- Pairs_Dim[1]
+    #   Num_Pairs
+    #
+    #   if (Num_Pairs == 0 || is.null(Num_Pairs) ){
+    #     Mu[i] <- NA
+    #     SD[i] <- NA
+    #
+    #   } else {
+    #
+    #     #Distance analysis of the pairs per cluster
+    #     Distances <- matrix(0L, nrow = Num_Pairs, ncol = 1)
+    #
+    #     for (j in 1:Num_Pairs) {
+    #       Distances[j] <- dist(rbind(B1[,Cl_Pairs[j,2]],B2[,Cl_Pairs[j,1]]))
+    #       Mu[i] <- mean(Distances)
+    #       SD[i] <- sd(Distances)
+    #     }
+    #   }
+    # }
+    #
+    # #Select the cluster with minimum distance mean
+    # Min_Cl <- which.min(Mu)
+    #
+    # #Cluster cells index
+    # Cl_Cells_Index<- which(Cluster$cluster ==Min_Cl)
+    #
+    # #Cluster cells subset
+    # Cl_Cells<- B2[,Cl_Cells_Index]
+    #
+    # #Cluster cells pairs
+    # Cl_Pairs_Index <- which(Pairs[,1]==Cl_Cells_Index[1])
+    # for (j in 2:length(Cl_Cells_Index) ) {
+    #   Cl_Pairs_Index <- c(Cl_Pairs_Index,which(Pairs[,1]==Cl_Cells_Index[j]))
+    # }
+    #
+    # #Subset of the pairs of the selected cluster
+    # Cl_Pairs <- Pairs[Cl_Pairs_Index,]
+    #
+    # return(list( "Selected Pairs" =Cl_Pairs, "Clusters" = Cluster, "Selected Cluster" = Min_Cl ) )
+#  }else{
 
-  # Clustering to select pairs
-  #Cluster <- kmeans(t(B2)[,1:10],Num_Clusters)
-  Cluster <- kmeans(t(B2),Num_Clusters)
+    if(Verbose)
+      cat("\n\nFitering pairs by quantiles")
 
-  #Initialization of variables for the correction
-  Pairs_Dim <- dim(Pairs)
-  Num_Pairs <- Pairs_Dim[1]
-  Pairs <- matrix(c(as.vector(Pairs[1:Num_Pairs,1]), as.vector(Pairs[1:Num_Pairs,2])), nrow = Num_Pairs)
-  #Samples <- Pairs
+    v1 <- t(B1[,Pairs[,2]])
+    v2 <- t(B2[,Pairs[,1]])
 
-  for (i in 1:Num_Clusters) {
+    d <- as.matrix(dist(rbind(v1,v2)))
 
-    #Cluster cells index
-    Cl_Cells_Index<- which(Cluster$cluster ==i)
+    d <- d[matrix(seq(from = 1, to = (nrow(v1)*2)), ncol = 2)]
 
-    #Cluster cells subset
-    Cl_Cells<- B2[,Cl_Cells_Index]
+    q <- quantile(d)
 
-    #Cluster cells pairs
-    Cl_Pairs_Index <- which(Pairs[,1]==Cl_Cells_Index[1])
-    for (j in 2:length(Cl_Cells_Index) ) {
-      Cl_Pairs_Index <- c(Cl_Pairs_Index,which(Pairs[,1]==Cl_Cells_Index[j]))
-    }
+    IQR <- q["75%"] - q["25%"]
 
-    #Subset of the pairs corresponding to the cluster
-    Cl_Pairs <- Pairs[Cl_Pairs_Index,]
+    outliers <- c(which(d < (q["25%"] - (1.5*IQR))), which(d > (q["75%"] + (1.5*IQR))))
+    #outliers <- c(which(d < q["25%"]), which(d > q["75%"]))
 
-    #Number of pairs for this cluster
-    Pairs_Dim <- dim(Cl_Pairs)
-    Num_Pairs <- Pairs_Dim[1]
-    Num_Pairs
+    selPairs <- Pairs[-outliers,]
 
-    if (Num_Pairs == 0 || is.null(Num_Pairs) ){
-      Mu[i] <- NA
-      SD[i] <- NA
+    return(list( "Selected Pairs" = selPairs, "Clusters" = NULL, "Selected Cluster" = NULL ) )
 
-    } else {
+#  }
 
-      #Distance analysis of the pairs per cluster
-      Distances <- matrix(0L, nrow = Num_Pairs, ncol = 1)
 
-      for (j in 1:Num_Pairs) {
-        Distances[j] <- dist(rbind(B1[,Cl_Pairs[j,2]],B2[,Cl_Pairs[j,1]]))
-        Mu[i] <- mean(Distances)
-        SD[i] <- sd(Distances)
-      }
-    }
-  }
-
-  #Select the cluster with minimum distance mean
-  Min_Cl <- which.min(Mu)
-
-  #Cluster cells index
-  Cl_Cells_Index<- which(Cluster$cluster ==Min_Cl)
-
-  #Cluster cells subset
-  Cl_Cells<- B2[,Cl_Cells_Index]
-
-  #Cluster cells pairs
-  Cl_Pairs_Index <- which(Pairs[,1]==Cl_Cells_Index[1])
-  for (j in 2:length(Cl_Cells_Index) ) {
-    Cl_Pairs_Index <- c(Cl_Pairs_Index,which(Pairs[,1]==Cl_Cells_Index[j]))
-  }
-
-  #Subset of the pairs of the selected cluster
-  Cl_Pairs <- Pairs[Cl_Pairs_Index,]
-
-  return(list( "Selected Pairs" =Cl_Pairs, "Clusters" = Cluster, "Selected Cluster" = Min_Cl ) )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
