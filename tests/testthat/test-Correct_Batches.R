e82edfe42519594621fe2dbcd7b6ed587c377613
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2020-12-21 16:22:38 +0900

    Revert "Update Batch_Correct unit_test"
    This reverts commit 23cc06bee40f4d07393e620b8e5ae25845b7df44.

--- tests/testthat/test-Correct_Batches.R
+++ tests/testthat/test-Correct_Batches.R
@@ -10,6 +10,6 @@ test_that("Correct_Batches works", {
   expect_equal(length(which(is.na(z$`Batches Integrated`))),0)
   expect_equal(length(z),2)
   expect_equal(names(z), c("B1/B2","Batches Integrated"))
-  expect_equal(z$`B1/B2`$`Corrected Query Batch`[1,1], 6.134769, tolerance = 0.0001)
+  expect_equal(z$`B1/B2`$`Corrected Query Batch`[1,1], 6.133837, tolerance = 0.0001)
   expect_equal( dim(z$`Batches Integrated`), dim(cbind(Batches$B1,Batches$B2) ))
 })
