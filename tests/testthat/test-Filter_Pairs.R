6b3b4b88531e13a3528c5bbc092d9d06142b6edf
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2020-12-25 11:54:21 +0900

    Update Filter Pairs unit test

--- tests/testthat/test-Filter_Pairs.R
+++ tests/testthat/test-Filter_Pairs.R
@@ -3,19 +3,14 @@ context("test-Filter_Pairs")
 set.seed(0)
 
 Batches <- SimBatches$batches
-z <- Correct_Batches(Batches, Num_Clusters = 3, Sampling = FALSE)
-#M2_Pairs_Data <- z$`B1/B2`$`Correction Data`$`Membership Data`$`Membership Correction Data`$`Membership 2`$`Pairs Selection Data`
-#M3_Pairs_Data <- z$`B1/B2`$`Correction Data`$`Membership Data`$`Membership Correction Data`$`Membership 3`$`Pairs Selection Data`
+z <- Correct_Batch(Batches$B1, Batches$B2, queNumCelltypes = 2, pairsFilter = TRUE)
+pairs <- z$`Correction Data`$`MNN Pairs`
 
 test_that("Filter_Pairs works", {
-  #expect_false(is.null(M2_Pairs_Data) || is.null(M3_Pairs_Data))
-  #expect_true( (length(M2_Pairs_Data) == 3) &&  (length(M3_Pairs_Data) == 3) )
-  #expect_equal( names(M2_Pairs_Data), c("Selected Pairs", "Clusters", "Selected Cluster") )
-  #expect_equal( names(M3_Pairs_Data), c("Selected Pairs", "Clusters", "Selected Cluster") )
-  #expect_equal(length(which(is.na(M2_Pairs_Data$`Selected Pairs`))),0)
-  #expect_equal(length(which(is.na(M3_Pairs_Data$`Selected Pairs`))),0)
-  #expect_equal(dim(M2_Pairs_Data$`Selected Pairs`), c(258, 2))
-  #expect_equal(dim(M3_Pairs_Data$`Selected Pairs`), c(680, 2))
-  #expect_true( (M2_Pairs_Data$`Selected Cluster` == 2) && (M3_Pairs_Data$`Selected Cluster` == 1) )
-  #expect_false(is.null(M2_Pairs_Data$Clusters) || is.null(M3_Pairs_Data$Clusters))
+  expect_false(is.null(pairs))
+  expect_true((nrow(pairs) == 5918) & (ncol(pairs) == 2))
+  expect_true(length(which(is.na(pairs))) == 0)
+  expect_true(length(unique(pairs[,1])) == 674 & length(unique(pairs[,2])) == 514)
+  expect_true(length(which(pairs[,1] == 1)) == 8)
+  expect_true(length(which(pairs[,1] == 689)) == 0)
 })
