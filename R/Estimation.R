71989263c2a60648ef225c7a8dc56ed26cd2ccf7
Martin de Jesus Loza Lopez <mloza@ifrec.osaka-u.ac.jp>
2021-01-20 22:03:30 +0900

    Add roxigen documentation to MedianBE function

--- R/Estimation.R
+++ R/Estimation.R
@@ -130,6 +130,21 @@ EkfBE <- function(refBatch, queBatch, pairs, sampling=NULL, numSamples= NULL, ve
 }
 
 
+#' Correction vector estimation
+#'
+#' @description Batch effect estimation using the MNNs pairs.
+#'
+#' @param refBatch Reference batch.
+#' @param queBatch Query batch.
+#' @param pairs A numerical matrix containing MNNs pairs cell indexes. First column corresponds to query batch cells.
+#'
+#' @return A list containing the estimated correction vector and the estimation data.
+#' The length of the correction vector is equal to the number of genes.
+#'
+#' @details The input batches must have the same number of genes. The model used on the estimation has the form of g_ref = g_que + be, where
+#' the batch effect is represented as a value added to the reference gene expression. The batch effect is estimated as
+#' the median of the gene expression difference among the reference and the query batch, e.g. Median(g_ref - g_que).
+#'
 MedianBE <- function(refBatch, queBatch, pairs ){
 
   # Model -> g_ref = g_que + be
