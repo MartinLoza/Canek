5b10d0147f7aede9aa7716d22f58e4a70b17e8f1
Diego Diez <diego10ruiz@gmail.com>
2021-03-08 15:48:56 +0900

    Fix code formatting.

--- R/Canek-package.R
+++ R/Canek-package.R
@@ -4,7 +4,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-## usethis namespace: end
 #' @import numbers
 #' @importFrom stats dist kmeans sd quantile
 #' @importFrom igraph cluster_louvain membership mst graph_from_adjacency_matrix as_adjacency_matrix
@@ -15,4 +14,5 @@
 #' @importFrom FNN get.knnx
 #' @importFrom batchelor cosineNorm
 #' @importFrom matrixStats rowMedians
+## usethis namespace: end
 NULL
