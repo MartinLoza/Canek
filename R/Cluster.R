5eae8bcfe814ace0407944c458859bb5854855fa
Diego Diez <diego10ruiz@gmail.com>
2021-03-13 15:52:25 +0900

    Move cluster related functions to Cluster.R.

---
+++ R/Cluster.R
@@ -0,0 +1,44 @@
+ClusterKMeans <- function(x, maxMem = 10, nMem = NULL, verbose = TRUE, usepam = FALSE) {
+  if(is.null(nMem)){
+    if(verbose)
+      cat("\n\nFinding number of memberships")
+
+    nMem <- pamk(x,
+                 krange = seq_len(maxMem),
+                 usepam = usepam)$nc
+
+    if (verbose)
+      cat(paste('\n\tNumber of memberships found:', nMem))
+  }
+
+  list(result = kmeans(x, nMem), nMem = nMem)
+}
+
+ClusterLouvain <- function(x, k = 10, verbose = TRUE) {
+  if (verbose)
+    cat("\n\nFinding number of memberships")
+
+  g <- bluster::makeSNNGraph(x, k = k)
+  res <- igraph::cluster_louvain(g)
+
+  memberships <- igraph::membership(res)
+  nMem <- length(unique(memberships))
+
+  if (verbose)
+    cat(paste('\n\tNumber of memberships found:', nMem))
+
+  centers <- CalculateCenters(x, memberships)
+
+  cluster <- list(cluster = memberships, centers = centers)
+  list(result = cluster, nMem = nMem)
+}
+
+CalculateCenters <- function(x, memberships) {
+  nMem <- length(unique(memberships))
+
+  centers <- sapply(seq_len(nMem), function(membership) {
+    colMeans(x[memberships == membership, ])
+  })
+  colnames(centers) <- seq_len(nMem)
+  t(centers)
+}
