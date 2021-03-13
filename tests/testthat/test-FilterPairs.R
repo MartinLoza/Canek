ce62262a8c40e9289784f73296da1c0533d6895a
Diego Diez <diego10ruiz@gmail.com>
2021-03-13 16:22:58 +0900

    Rewrite tests to obtain more informative message about nature of error.

--- tests/testthat/test-FilterPairs.R
+++ tests/testthat/test-FilterPairs.R
@@ -8,9 +8,11 @@ pairs <- z$`B2/B1`$`Correction Data`$`MNN Pairs`
 
 test_that("FilterPairs works", {
   expect_false(is.null(pairs))
-  expect_true((nrow(pairs) == 5918) & (ncol(pairs) == 2))
-  expect_true(length(which(is.na(pairs))) == 0)
-  expect_true(length(unique(pairs[,1])) == 514 & length(unique(pairs[,2])) == 674)
-  expect_true(length(which(pairs[,2] == 1)) == 8)
-  expect_true(length(which(pairs[,2] == 689)) == 0)
+  expect_equal(nrow(pairs), 5918)
+  expect_equal(ncol(pairs), 2)
+  expect_equal(length(which(is.na(pairs))), 0)
+  expect_equal(length(unique(pairs[, 1])), 514)
+  expect_equal(length(unique(pairs[, 2])), 674)
+  expect_equal(length(which(pairs[,2] == 1)), 8)
+  expect_equal(length(which(pairs[,2] == 689)), 0)
 })
