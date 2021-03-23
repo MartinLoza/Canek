08e8b476897cfc4e4b8d04174edb7cec3eeaa5e0
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2021-03-23 09:57:44 +0900

    Add unit-test for clustering

---
+++ tests/testthat/test-Clustering.R
@@ -0,0 +1,21 @@
+set.seed(0)
+
+Batches <- SimBatches$batches
+x <- CorrectBatches(Batches, queNumCelltypes = 2, doCosNorm = TRUE, clusterMethod = "kmeans", debug = TRUE)
+y <- CorrectBatches(Batches, doCosNorm = TRUE, clusterMethod = "louvain", debug = TRUE)
+dataKmeans <- x$`B2/B1`$`Correction Data`$Clusters
+dataLouvain <- y$`B2/B1`$`Correction Data`$Clusters
+
+test_that("Clustering with kmeans works", {
+  expect_false(is.null(dataKmeans))
+  expect_equal(names(dataKmeans), c("cluster", "centers"))
+  expect_equal(as.integer(table(dataKmeans$cluster)), c(38, 593))
+  expect_equal(dim(dataKmeans$centers), c(2, 10))
+})
+
+test_that("Clustering with louvain works", {
+  expect_false(is.null(dataLouvain))
+  expect_equal(names(dataLouvain), c("cluster", "centers"))
+  expect_equal(as.integer(table(dataLouvain$cluster)), c(107,162,146,178,38))
+  expect_equal(dim(dataLouvain$centers), c(5, 10))
+})
