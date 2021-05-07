a04782dc5d1ca89d2648a140507714aedfe26c49
Martin de Jesus Loza Lopez <mloza@ifrec.osaka-u.ac.jp>
2021-05-07 23:01:18 +0900

    Add CosNorm function

--- R/Conversions.R
+++ R/Conversions.R
@@ -1,9 +1,16 @@
 
 ##Rad2Deg##
 #Convert radians to degrees
-# INPUT : Angle in radiasn
+# INPUT : Angle in radians
 #
 # OUTPUT : Angle in degree
 Rad2Deg <- function(Angle_rad = NULL){
   return(Angle_rad*180/(pi))
+}
+
+## Cosine Normalization
+CosNorm <- function(data){
+  sizeFactor <- sqrt(colSums(data^2))
+  normData <- t(t(data)/sizeFactor)
+  return(normData)
 }
