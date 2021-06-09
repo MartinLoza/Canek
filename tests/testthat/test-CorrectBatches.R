0243bdef101f0031a35750888949e022902d945b
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2021-06-09 17:41:29 +0900

    Update unit-test,

--- tests/testthat/test-CorrectBatches.R
+++ tests/testthat/test-CorrectBatches.R
@@ -8,5 +8,5 @@ test_that("CorrectBatches works", {
   expect_equal(length(which(is.na(z))),0)
   expect_equal(dim(z), dim(cbind(Batches$B1,Batches$B2)))
   expect_true(ncol(z) == 1579 & nrow(z) == 500)
-  expect_equal(z[1,1], 6.664089, tolerance = 0.0001)
+  expect_equal(z[1,1], 6.133852, tolerance = 0.0001)
 })
