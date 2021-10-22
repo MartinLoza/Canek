7569f3f515723aaed933362aa6f307c0ec32beef
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2021-10-23 07:31:10 +0900

    Add only-zero rows filtering functions.

--- R/Filtering.R
+++ R/Filtering.R
@@ -1,6 +1,4 @@
-
-
-#' Title PairsFiltering
+#' PairsFiltering
 #'
 #' Function to filter MNNs pairs
 #'
@@ -31,6 +29,55 @@ PairsFiltering <- function(refBatch,queBatch, pairs, verbose = FALSE){
     outliers <- c(which(d < (q["25%"] - (1.5*IQR))), which(d > (q["75%"] + (1.5*IQR))))
 
     return(pairs[-outliers,])
+}
+
+#' FilterRowZeros
+#'
+#' @param matrix Numeric matrix to filter.
+#' @param rowZeros If known, zeros row indexes.
+#'
+#' @return Matrix with no zeros rows.
+FilterRowZeros <- function(matrix = NULL, rowZeros = NULL){
+
+  ## Init
+  rNames <- rownames(matrix)
+
+  if(length(rNames) == 0)
+    rNames <- seq(1:nrow(matrix))
+
+  if(is.null(rowZeros))
+    rowZeros <- WhichRowZeros(matrix = matrix, idxReturn = TRUE)$idx
+
+  matrix <- matrix[-zeroIdx,, drop = FALSE]
+
+  return(matrix)
+}
+
+#' WhichRowZeros
+#'
+#' @param matrix Numeric matrix to filter.
+#' @param idxReturn Return the zeros row indexes.
+#'
+#' @return A list with the zeros rows' names and their indexes.
+WhichRowZeros <- function(matrix = NULL, idxReturn = FALSE){
+
+  ## Init
+  rNames <- rownames(matrix)
+  results <- list()
+
+  if(length(rNames) == 0)
+    rNames <- seq(1:nrow(matrix))
+
+  ## Get row zeros names
+  idx <- which(matrixStats::rowSums2(matrix) == 0)
+  rowZeros <- rNames[idx]
+
+  results[["names"]] <- rowZeros
+
+  if(idxReturn)
+    results[["idx"]] <- idx
+
+  return(results)
 }
 
 
