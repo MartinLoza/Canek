9ba2f5aa7925de5b95b85ae44c8a9a5ef4696ffc
Diego Diez <diego10ruiz@gmail.com>
2022-11-23 15:42:03 +0900

    Fixes to new Seurat pipeline.

--- R/RunCanek.R
+++ R/RunCanek.R
@@ -8,7 +8,8 @@
 #' @param assay assay used for Seurat objects (default: RNA).
 #' @param features optional vector of features to use for correction.
 #' @param selection.method method used for FindVariableFeatures on Seurat objects when features is NULL.
-#' @param fvf.nfeatures function used to collapse variable features from different batches. Default is intersect.
+#' @param nfeatures  number of features returned by SelectIntegrationFeatures.
+#' @param fvf.nfeatures number of features returned by FindVariableFeatures.
 #' @param debug whether to store information about correction vector.
 #' @param ... additional arguments passed down to methods.
 #'
@@ -22,17 +23,18 @@ RunCanek <- function(x, ...) {
 
 #' @rdname RunCanek
 #' @export
-RunCanek.Seurat <- function(x, batches = NULL, slot = "data", assay = "RNA", features = NULL, selection.method = "vst", fvf.nfeatures = 2000, debug = FALSE, ...) {
+RunCanek.Seurat <- function(x, batches = NULL, slot = "data", assay = "RNA", features = NULL, selection.method = "vst", nfeatures = 2000, fvf.nfeatures = 2000, debug = FALSE, ...) {
   #x <- Seurat::SplitObject(x, split.by=batches)
   #RunCanek(x, slot = slot, assay = assay, features = features, selection.method = selection.method, fvf.nfeatures = fvf.nfeatures, debug = debug, ...)
 
   obj <- Seurat::DietSeurat(x, counts = TRUE, data = TRUE, scale.data = FALSE, assays = assay)
+  obj <- Seurat::SplitObject(obj, split.by = batches)
 
   if (is.null(features)) {
     features <- Seurat::SelectIntegrationFeatures(obj, nfeatures = nfeatures, fvf.nfeatures = fvf.nfeatures, selection.method = selection.method, verbose = FALSE)
   }
 
-  counts <- lapply(x, function(xx) {
+  counts <- lapply(obj, function(xx) {
     Seurat::GetAssayData(xx, slot = slot, assay = assay)[features, ]
   })
 
@@ -77,7 +79,7 @@ RunCanek.list <- function(x, ...) {
   if (length(objtype) != 1) stop("Required list of identical object types.")
   switch(objtype,
     #"Seurat" = RunCanekSeurat(x, ...),
-    #"SingleCellExperiment" = RunCanekSingleCellExperiment(x, ...),
+    "SingleCellExperiment" = RunCanekSingleCellExperiment(x, ...),
     "matrix" = CorrectBatches(x, ...)
   )
 
