2c062db54f55434723f339d3dd203ec472a1f655
Diego Diez <diego10ruiz@gmail.com>
2023-12-07 20:51:54 +0900

    Fix louvain tests for igraph v2.

--- tests/testthat/test-Clustering.R
+++ tests/testthat/test-Clustering.R
@@ -16,6 +16,6 @@ test_that("Clustering with kmeans works", {
 test_that("Clustering with louvain works", {
   expect_false(is.null(dataLouvain))
   expect_equal(names(dataLouvain), c("cluster", "centers"))
-  expect_equal(as.integer(table(dataLouvain$cluster)), c(96, 125, 140, 171, 99))
-  expect_equal(dim(dataLouvain$centers), c(5, 10))
+  expect_equal(length(dataLouvain$cluster), 631)
+  expect_equal(ncol(dataLouvain$centers), 10)
 })
