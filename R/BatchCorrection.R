9130274b08e3a64dceb6c09a5011084406874a47
Martin Loza <martin.loza.lopez@gmail.com>
2026-07-17 19:34:10 +0900

    Add \donttest examples to CorrectBatches and CorrectBatch documentation to prevent execution during checks

--- R/BatchCorrection.R
+++ R/BatchCorrection.R
@@ -63,6 +63,7 @@
 #' }
 #'
 #' @examples
+#' \donttest{
 #' Batches <- SimBatches$batches
 #' z <- CorrectBatches(Batches)
 #'
@@ -70,6 +71,7 @@
 #' plot(Uncorrected_PCA$x[,1:2])
 #' Corrected_PCA <- prcomp(t(z))
 #' plot(Corrected_PCA$x[,1:2])
+#' }
 #'
 #' @return A list containing the integrated datasets as matrix and the correction data .
 #' @export
@@ -340,6 +342,7 @@ CorrectBatches <- function(lsBatches, hierarchical = TRUE,
 #' }
 #'
 #' @examples
+#' \donttest{
 #' x <- SimBatches$batches[[1]]
 #' y <- SimBatches$batches[[2]]
 #' z <- CorrectBatch(x, y)
@@ -349,6 +352,7 @@ CorrectBatches <- function(lsBatches, hierarchical = TRUE,
 #' plot(Uncorrected_PCA$x[,1:2])
 #' Corrected_PCA <- prcomp(t(cbind(x,z$`Corrected Query Batch`)))
 #' plot(Corrected_PCA$x[,1:2])
+#' }
 #'
 #' @return A list containing the input batches, the corrected query batch, and the correction data
 #' @export
